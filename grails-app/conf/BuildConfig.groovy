10d2dd4b7ec25830f94a055dbc39ff745316cdd0
Pablo Pazos <pablo.pazos@cabolabs.com>
2014-08-18 14:03:57 -0300

    run on port 8090 by default

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -5,6 +5,7 @@ grails.project.test.reports.dir = "target/test-reports"
 grails.project.target.level = 1.6
 grails.project.source.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
+grails.server.port.http = 8090
 
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
