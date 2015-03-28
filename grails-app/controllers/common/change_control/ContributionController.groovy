7c9cab519574f1b5e73a51e72e45e393db237f9f
Pablo Pazos <pablo.pazos@cabolabs.com>
2015-03-28 04:55:20 -0300

    unused method removed

--- grails-app/controllers/common/change_control/ContributionController.groovy
+++ grails-app/controllers/common/change_control/ContributionController.groovy
@@ -21,21 +21,6 @@ class ContributionController {
            contributionInstanceTotal: cnt]
    }
 
-   def create() {
-      [contributionInstance: new Contribution(params)]
-   }
-
-   def save() {
-      def contributionInstance = new Contribution(params)
-      if (!contributionInstance.save(flush: true)) {
-         render(view: "create", model: [contributionInstance: contributionInstance])
-         return
-      }
-
-      flash.message = message(code: 'default.created.message', args: [message(code: 'contribution.label', default: 'Contribution'), contributionInstance.id])
-      redirect(action: "show", id: contributionInstance.id)
-   }
-
    def show(Long id) {
       def contributionInstance = Contribution.get(id)
       if (!contributionInstance) {
@@ -47,62 +32,4 @@ class ContributionController {
       [contributionInstance: contributionInstance]
    }
 
-   def edit(Long id) {
-      def contributionInstance = Contribution.get(id)
-      if (!contributionInstance) {
-         flash.message = message(code: 'default.not.found.message', args: [message(code: 'contribution.label', default: 'Contribution'), id])
-         redirect(action: "list")
-         return
-      }
-
-      [contributionInstance: contributionInstance]
-   }
-
-   def update(Long id, Long version) {
-      def contributionInstance = Contribution.get(id)
-      if (!contributionInstance) {
-         flash.message = message(code: 'default.not.found.message', args: [message(code: 'contribution.label', default: 'Contribution'), id])
-         redirect(action: "list")
-         return
-      }
-
-      if (version != null) {
-         if (contributionInstance.version > version) {
-            contributionInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
-                    [message(code: 'contribution.label', default: 'Contribution')] as Object[],
-                    "Another user has updated this Contribution while you were editing")
-            render(view: "edit", model: [contributionInstance: contributionInstance])
-            return
-         }
-      }
-
-      contributionInstance.properties = params
-
-      if (!contributionInstance.save(flush: true)) {
-         render(view: "edit", model: [contributionInstance: contributionInstance])
-         return
-      }
-
-      flash.message = message(code: 'default.updated.message', args: [message(code: 'contribution.label', default: 'Contribution'), contributionInstance.id])
-      redirect(action: "show", id: contributionInstance.id)
-   }
-
-   def delete(Long id) {
-      def contributionInstance = Contribution.get(id)
-      if (!contributionInstance) {
-         flash.message = message(code: 'default.not.found.message', args: [message(code: 'contribution.label', default: 'Contribution'), id])
-         redirect(action: "list")
-         return
-      }
-
-      try {
-         contributionInstance.delete(flush: true)
-         flash.message = message(code: 'default.deleted.message', args: [message(code: 'contribution.label', default: 'Contribution'), id])
-         redirect(action: "list")
-      }
-      catch (DataIntegrityViolationException e) {
-         flash.message = message(code: 'default.not.deleted.message', args: [message(code: 'contribution.label', default: 'Contribution'), id])
-         redirect(action: "show", id: id)
-      }
-   }
 }
