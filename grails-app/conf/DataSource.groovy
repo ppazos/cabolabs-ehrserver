4ac1617a0712525e7b3acd4c1ef5a0093e5e9f61
Juan Carlos <casiodbx@hotmail.com>
2016-05-14 10:08:15 +0200

    Correcion Pull Reques issue 325

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -3,7 +3,7 @@ dataSource {
     driverClassName = 'com.mysql.jdbc.Driver'
     dialect = org.hibernate.dialect.MySQL5InnoDBDialect
     username = "root"
-    password = "root"
+    password = ""
 }
 hibernate {
     cache.use_second_level_cache = true
