c735f2eead9692519b8831894e379c49b54e0f12
Jorge Aguilera <jagedn@gmail.com>
2016-07-19 12:40:56 +0200

    use openEHR repository from github

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -52,6 +52,9 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
+		
+		mavenRepo "https://raw.github.com/jagedn/openEHR-OPT/mvn-repo/"
+		
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
