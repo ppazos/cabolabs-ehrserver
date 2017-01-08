1ffd1cd83b175184482abe78cd066614ae06fef0
Pablo Pazos <pablo.pazos@cabolabs.com>
2017-01-08 15:11:12 -0300

    fixes #528

--- grails-app/domain/com/cabolabs/ehrserver/query/datatypes/DataCriteriaDV_CODED_TEXT.groovy
+++ grails-app/domain/com/cabolabs/ehrserver/query/datatypes/DataCriteriaDV_CODED_TEXT.groovy
@@ -69,7 +69,9 @@ class DataCriteriaDV_CODED_TEXT extends DataCriteria {
        def codes = [:]
        def code
        
-       optMan.getNode(archetypeId, path + '/defining_code')?.xmlNode.code_list.each {
+       // if the coded text doesn't have a constraint, xmlNode is null
+       // https://github.com/ppazos/cabolabs-ehrserver/issues/528
+       optMan.getNode(archetypeId, path + '/defining_code')?.xmlNode?.code_list.each {
           
           code = it.text()
           codes[code] = optMan.getText(archetypeId, code) // at00XX -> name
