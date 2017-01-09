1ed8de61ddd8643f3555e1d660e01efc97eecb55
Pablo Pazos <pablo.pazos@cabolabs.com>
2017-01-09 03:26:25 -0300

    added fixes to test

--- test/integration/com/cabolabs/ehrserver/data/DataIndexerServiceIntegrationSpec.groovy
+++ test/integration/com/cabolabs/ehrserver/data/DataIndexerServiceIntegrationSpec.groovy
@@ -6,10 +6,15 @@ import com.cabolabs.ehrserver.ehr.clinical_documents.CompositionIndex
 import com.cabolabs.ehrserver.openehr.common.change_control.Contribution
 import com.cabolabs.ehrserver.openehr.common.change_control.VersionedComposition
 import com.cabolabs.ehrserver.openehr.common.change_control.Version
+import com.cabolabs.ehrserver.openehr.common.generic.PatientProxy
 import com.cabolabs.ehrserver.openehr.ehr.Ehr
 import grails.util.Holders
 import groovy.io.FileType
 import com.cabolabs.ehrserver.ehr.clinical_documents.OperationalTemplateIndex
+import com.cabolabs.ehrserver.ehr.clinical_documents.OperationalTemplateIndexShare
+
+import com.cabolabs.security.Organization
+import java.util.logging.Logger
 
 class DataIndexerServiceIntegrationSpec extends IntegrationSpec {
 
@@ -18,11 +23,23 @@ class DataIndexerServiceIntegrationSpec extends IntegrationSpec {
    
    private static String PS = System.getProperty("file.separator")
    
+   def log = Logger.getLogger('com.cabolabs.ehrserver.data.DataIndexerServiceIntegrationSpec')
+   
    
    def setup()
    {
       // used by the service, mock the version repo where commits are stored
       //Holders.config.app.version_repo = "test"+ PS +"resources"+ PS +"temp_versions" + PS
+      def uid = '11111111-1111-1111-1111-111111111111'
+      def ehr = new Ehr(
+         uid: uid, // the ehr id is the same as the patient just to simplify testing
+         subject: new PatientProxy(
+            value: uid
+         ),
+         organizationUid: Organization.get(1).uid
+      )
+    
+      if (!ehr.save()) println ehr.errors
    }
 
    def cleanup()
@@ -45,22 +62,38 @@ class DataIndexerServiceIntegrationSpec extends IntegrationSpec {
 
          def versionsXML = new File('test'+ PS +'resources'+ PS +'commit'+ PS +'test_commit_1.xml').text
          versionsXML = versionsXML.replaceAll('\\[PATIENT_UID\\]', ehr.subject.value)
-         
+
          def slurper = new XmlSlurper(false, false)
          def parsedVersions = slurper.parseText(versionsXML)
-         
+log.warn "pre commit"
          assert CompositionIndex.count() == 0
          xmlService.processCommit(ehr, parsedVersions, 'CaboLabs EMR', new Date(), 'House, MD.')
+log.warm "post commit"
          assert CompositionIndex.count() == 1
-         
+
       when:
+println "E"
+         def compoIndex = CompositionIndex.findByDataIndexed(false)
+         assert compoIndex != null
          assert CompositionIndex.countByDataIndexed(false) == 1
+         
+         // The template for the instance is loaded
+         assert OperationalTemplateIndex.countByTemplateId( compoIndex.templateId ) == 1
+         
+         println compoIndex.organizationUid
+         assert Organization.get(1).uid == compoIndex.organizationUid
+         
+         // OperationalTemplateIndexer should generated the shares on bootstrap from indexAll
+         println "shares "+ OperationalTemplateIndexShare.list()
+         
          dataIndexerService.generateIndexes( CompositionIndex.findByDataIndexed(false) )
+println "F"
          assert CompositionIndex.countByDataIndexed(true) == 1
           
       then:
          // commit was ok
          notThrown Exception // this shouldn't throw any exceptions
+      
          assert Contribution.count() == 1
          assert Version.count() == 1
          assert VersionedComposition.count() == 1
