c890cd309e66749736e7206a46c795ba6ca4b604
Pablo Pazos <pablo.pazos@cabolabs.com>
2017-09-13 15:38:14 -0300

    added configuration item model for #710

---
+++ grails-app/domain/com/cabolabs/ehrserver/conf/ConfigurationItem.groovy
@@ -0,0 +1,66 @@
+package com.cabolabs.ehrserver.conf
+
+class ConfigurationItem {
+
+   String key
+   String value
+   String type
+   boolean blank = false
+   String description
+   
+   /* MySQL reserved words */
+   static mapping = {
+      key column: "config_key"
+      value column: "config_value"
+      type column: "config_type"
+   }
+
+   static constraints = {
+      key (unique: true, nullable: false, blank: false)
+      value validator: { val, obj, errors ->
+         if (!obj.blank && !val) errors.rejectValue('value', 'emptyValueNotAllowedBlankIsFalse')
+         switch (obj.type) {
+            case 'string':
+               // any valid string will validate
+            break
+            case 'number':
+               // TODO: need to consider decimal points depending on the current locale
+               // the way to check the number was parsed OK is to serialize and compare with the original value
+               // https://gist.github.com/ppazos/9dfb8b856ddd14518de0a4111231a095
+               def format = java.text.NumberFormat.getInstance()
+               try
+               {
+                  format.parse(val)
+               }
+               catch (Exception e)
+               {
+                  errors.rejectValue('value', 'invalidNumber')
+               }
+            break
+            case 'boolean':
+               if (!['true','false'].contains(val))
+            break
+            case 'url':
+               if (!(val =~ /\b(https?|ftp|file):\\/\\/[-a-zA-Z0-9+&@#\\/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#\\/%=~_|]/))
+               {
+                  errors.rejectValue('value', 'invalidURL')
+               }
+            break
+            case 'uuid':
+               if (!(val =~ /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/))
+               {
+                  errors.rejectValue('value', 'invalidUUID')
+               }
+            break
+            case 'oid':
+               if (!(val =~ /^([0-2])(\\.([1-9]([0-9])*))*$/))
+               {
+                  errors.rejectValue('value', 'invalidOID')
+               }
+            break
+         }
+      }      
+      type(inList:['string', 'number', 'boolean', 'url', 'uuid', 'oid'])
+      description(nullable: false)
+   }
+}
---
+++ test/unit/com/cabolabs/ehrserver/conf/ConfigurationItemSpec.groovy
@@ -0,0 +1,108 @@
+package com.cabolabs.ehrserver.conf
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+import grails.test.mixin.TestMixin
+import grails.test.mixin.support.GrailsUnitTestMixin
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestMixin(GrailsUnitTestMixin)
+@TestFor(ConfigurationItem)
+class ConfigurationItemSpec extends Specification {
+
+   def setup() {
+   }
+
+   def cleanup() {
+   }
+
+   void "test validation OK"()
+   {
+      when:
+         def conf = new ConfigurationItem(key:'a.b.c', value:'abc', type:'string', blank:false, description:'my config item')
+      then:
+         conf.validate()
+   }
+   
+   void "test validation OK number"()
+   {
+      when:
+         def conf = new ConfigurationItem(key:'a.b.c', value:'123.5', type:'number', blank:false, description:'my config item')
+      then:
+         conf.validate()
+   }
+   
+   void "test validation OK URL"()
+   {
+      when:
+         def conf = new ConfigurationItem(key:'a.b.c', value:'https://cabolabs.com', type:'url', blank:false, description:'my config item')
+      then:
+         conf.validate()
+   }
+   
+   void "test validation blank value error"()
+   {
+      when:
+         def conf = new ConfigurationItem(key:'a.b.c', value:'', type:'string', blank:false, description:'my config item')
+      then:
+         !conf.validate()
+   }
+   
+   void "test validation null value error"()
+   {
+      when:
+         def conf = new ConfigurationItem(key:'a.b.c', value:'', type:'string', blank:false, description:'my config item')
+      then:
+         !conf.validate()
+   }
+   
+   void "test validation wrong type error"()
+   {
+      when:
+         def conf = new ConfigurationItem(key:'a.b.c', value:'abc', type:'date', blank:false, description:'my config item')
+      then:
+         !conf.validate()
+   }
+   
+   void "test validation value type mismatch boolean"()
+   {
+      when:
+         def conf = new ConfigurationItem(key:'a.b.c', value:'123', type:'boolean', blank:false, description:'my config item')
+      then:
+          conf.validate()
+   }
+   
+   void "test validation value type mismatch number"()
+   {
+      when:
+         def conf = new ConfigurationItem(key:'a.b.c', value:'abc', type:'number', blank:false, description:'my config item')
+      then:
+         !conf.validate()
+   }
+   
+   void "test validation value type mismatch UUID"()
+   {
+      when:
+         def conf = new ConfigurationItem(key:'a.b.c', value:'abc', type:'uuid', blank:false, description:'my config item')
+      then:
+         !conf.validate()
+   }
+   
+   void "test validation value type mismatch OID"()
+   {
+      when:
+         def conf = new ConfigurationItem(key:'a.b.c', value:'abc', type:'oid', blank:false, description:'my config item')
+      then:
+         !conf.validate()
+   }
+   
+   void "test validation value type mismatch URL"()
+   {
+      when:
+         def conf = new ConfigurationItem(key:'a.b.c', value:'abc', type:'url', blank:false, description:'my config item')
+      then:
+         !conf.validate()
+   }
+}
