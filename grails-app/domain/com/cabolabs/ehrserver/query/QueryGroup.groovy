91efab653fa94b7cf7c00759031b2076e0dd508d
Pablo Pazos Gutierrez <pablo.pazos@cabolabs.com>
2018-09-21 00:09:29 -0300

    fixes #986

--- grails-app/domain/com/cabolabs/ehrserver/query/QueryGroup.groovy
+++ grails-app/domain/com/cabolabs/ehrserver/query/QueryGroup.groovy
@@ -14,37 +14,40 @@ class QueryGroup {
    static mapping = {
      organizationUid index: 'org_uid_idx'
    }
-   
+
    /*
     * Returns the number of EHRs that match with each single query.
     */
    def executeCount(String organizationUid)
    {
       def queries = Query.findAllByQueryGroup(this)
-      
+
       // http://docs.grails.org/2.5.6/guide/async.html
       def tasks = []
       int max = Ehr.countByOrganizationUid(organizationUid)
       queries.each { query ->
-         
-         tasks << task {
-            def res = [:]
-            println "task "+ query.name
-            Query.withTransaction {
-               res[(query.uid)] = query.executeComposition(null, null, null, organizationUid, max, 0, null, null, false, true)
+
+         if (query.type == 'composition')
+         {
+            tasks << task {
+               def res = [:]
+               println "task "+ query.name
+               Query.withTransaction {
+                  res[(query.uid)] = query.executeComposition(null, null, null, organizationUid, max, 0, null, null, false, true)
+               }
+               return res
             }
-            return res
          }
       }
-      
+
       def res = [:]
       def all_results = waitAll(tasks)
-      
+
       all_results.each { resMap -> // has one key and the list of results in that key
          // returns the first and only entry
          def entry = resMap.find{ key, value -> true }
          res[entry.key] = entry.value.size()
-         
+
          /*
          println resMap.getClass()
          println resMap.keySet()
