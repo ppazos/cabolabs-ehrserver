c0805249e47ca6152b694f8163e55a4f0efb8a79
Pablo Pazos Gutierrez <pablo.pazos@cabolabs.com>
2019-05-03 04:47:58 -0300

    initial notifications menu for #1020

--- grails-app/assets/javascripts/notification.js
+++ grails-app/assets/javascripts/notification.js
@@ -4,18 +4,47 @@ function notification_get( get_not_url, dismiss_url, forSection )
    $.get( get_not_url, { forSection: forSection }, 'json')
       .done(function(data) {
         //console.log(data);
-        
+
+        if (data.length > 0)
+        {
+           $('#top-notifications-menu > a').append('<span class="badge badge-notification">'+ data.length +'</span>');
+        }
+
         notifications = '';
         $.each( data, function( i, notif ) {
-        
-          notifications += '<div class="alert alert-info alert-dismissible global" role="alert" data-id="'+ notif.id +'"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+ notif.text +'</div>';
+
+           // TODO: add  class="dismissed" to the LIs of notifications that were dismissed
+          $('#top-notifications-menu .drop-content').append('<li><div class="col-md-11">'+ notif.text +'<br/><span class="text-muted">'+ notif.dateCreated +'</span></div><div class="col-md-1 text-right"><a href="#" class="dismiss" data-id="'+ notif.id +'"><i class="fa fa-dot-circle-o"></i></a></div></li>');
+
+          //notifications += '<div class="alert alert-info alert-dismissible global" role="alert" data-id="'+ notif.id +'"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+ notif.text +'</div>';
         });
-        
-        $('body').append('<div class="global_alert_container">'+ notifications +'</div>');
+
+        //$('body').append('<div class="global_alert_container">'+ notifications +'</div>');
+
+        $('.dismiss').on('click', function (evn) {
+           console.log('id', $(this).data('id'));
+           // TODO: dismiss with the new notifications on the menu
+
+           var notif_dom = $(this).closest('li');
+
+           $.get( dismiss_url, { id: $(this).data('id') }, 'json')
+           .done(function(data) {
+             console.log(data);
+             // TODO: update the DOM adding the dismissed class to the LI
+             console.log(notif_dom);
+             notif_dom.addClass('dismissed');
+           })
+           .fail(function() {
+             //alert( "error" );
+           })
+           .always(function() {
+             //alert( "finished" );
+           });
+        });
 
         $('.alert').on('close.bs.alert', function (evn) {
            console.log($(evn.target).data('id'));
-           
+
            $.get( dismiss_url, { id: $(evn.target).data('id') }, 'json')
            .done(function(data) {
              console.log(data);
@@ -35,4 +64,3 @@ function notification_get( get_not_url, dismiss_url, forSection )
         //alert( "finished" );
       });
 }
-
--- grails-app/views/layouts/admin.gsp
+++ grails-app/views/layouts/admin.gsp
@@ -190,6 +190,33 @@
              :
              <g:link controller="organization" action="show" id="${session.organization.uid}" style="padding: 0; display: inline;">${session.organization.name}</g:link>
            </li>
+           <li class="dropdown" id="top-notifications-menu">
+             <a class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="fa fa-bell fa-fw"></i></a>
+             <ul class="dropdown-menu notify-drop">
+                <div class="notify-drop-title">
+                	<div class="row">
+                		<div class="col-md-6 col-sm-6 col-xs-6">Notifications</div>
+                		<div class="col-md-6 col-sm-6 col-xs-6 text-right"><a href="" class="rIcon allRead" data-tooltip="tooltip" data-placement="bottom" title="Dismiss all"><i class="fa fa-dot-circle-o"></i></a></div>
+                	</div>
+                </div>
+                <!-- end notify title -->
+                <!-- notify content -->
+                <div class="drop-content">
+                  <!--
+                	<li>
+                		<div class="col-md-3 col-sm-3 col-xs-3"><div class="notify-img"><img src="http://placehold.it/45x45" alt=""></div></div>
+                		<div class="col-md-9 col-sm-9 col-xs-9 pd-l0"><a href="">Ahmet</a> yorumladı. <a href="">Çicek bahçeleri...</a> <a href="" class="rIcon"><i class="fa fa-dot-circle-o"></i></a>
+                		<hr>
+                		<p class="time">Şimdi</p>
+                		</div>
+                	</li>
+                  -->
+                </div>
+                <div class="notify-drop-footer text-center">
+                	<!--<a href=""><i class="fa fa-eye"></i> View all notifications</a>-->
+                </div>
+              </ul>
+           </li>
            <li class="dropdown" id="top-user-menu">
              <a class="dropdown-toggle" data-toggle="dropdown" href="#">
                <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
@@ -216,6 +243,13 @@
            </li>
            <!-- /.dropdown -->
           </ul>
+
+          <script>
+          /* avoids closing the notifications drop down menu on click */
+          $(document).on('click', '#top-notifications-menu .dropdown-menu', function (e) {
+            e.stopPropagation();
+          });
+          </script>
 
           <!-- LEFT MENU -->
           <div class="navbar-default sidebar" role="navigation">
